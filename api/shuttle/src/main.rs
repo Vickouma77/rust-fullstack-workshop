2fdb789a41ab9bf8c0ebebafd8ff1bea07a117b6
Vickouma77 <Vickprestone20@gmail.com>
2025-02-10 17:51:08 +0300

    feat(shuttle): add database connection
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -6,11 +6,16 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
-#[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
-    };
+#[allow(unused)]
+fn main() {
+    #[shuttle_runtime::main]
+    async fn actix_web(
+        #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+        let config = move |cfg: &mut ServiceConfig| {
+            cfg.service(hello_world);
+        };
 
-    Ok(config.into())
+        Ok(config.into())
+    }
 }
