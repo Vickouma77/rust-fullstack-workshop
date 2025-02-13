7710960895b9b581fef1cc8dac6fe66c9c581dbd
Vickouma77 <Vickprestone20@gmail.com>
2025-02-13 16:16:29 +0300

    refactor(api): move endpoints to a library
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,29 +1,16 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::web::ServiceConfig;
+use api_lib::health::{hello_world, version};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::Executor;
 
 
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
-}
-
-
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting version");
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
-    result.unwrap_or_else(|e| format!("Error: {:?}", e))
-}
-
 #[shuttle_runtime::main]
 async fn actix_web(
-    #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
 
+    // initialize the database if not already initialized
     pool.execute(include_str!("../../db/schema.sql"))
         .await
         .map_err(CustomError::new)?;
@@ -33,7 +20,6 @@ async fn actix_web(
     let config = move |cfg: &mut ServiceConfig| {
         cfg.app_data(pool).service(hello_world).service(version);
     };
-
 
     Ok(config.into())
 }
