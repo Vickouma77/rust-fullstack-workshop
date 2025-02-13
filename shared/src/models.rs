20c8981216b71e6f264061b788b8537888db47a8
Vickouma77 <Vickprestone20@gmail.com>
2025-02-13 18:17:41 +0300

    perf(shared): add serde dependency and derive traits
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,3 +1,6 @@
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Film {
     // uuids as ids
     pub id: uuid::Uuid,
@@ -9,4 +12,12 @@ pub struct Film {
     pub poster: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
+pub struct CreateFilm {
+    pub title: String,
+    pub director: String,
+    pub year: u16,
+    pub poster: String,
 }
