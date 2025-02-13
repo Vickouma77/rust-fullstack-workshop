1f5570f5c6f01aa488112aca379c2fde252d587f
Vickouma77 <Vickprestone20@gmail.com>
2025-02-13 16:23:42 +0300

    perf(api): add health check endpoints
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -1,13 +1,10 @@
-use actix_web::get;
-#[get("/")]
-async fn hello_world() -> &'static str {
-    "Hello World!"
+use actix_web::{get, HttpResponse};
+
+
+#[get("/health")]
+async fn health() -> HttpResponse {
+    HttpResponse::Ok()
+        .append_header(("version", "0.0.1"))
+        .finish()
 }
-#[get("/version")]
-async fn version(db: actix_web::web::Data<sqlx::PgPool>) -> String {
-    tracing::info!("Getting version");
-    let result: Result<String, sqlx::Error> = sqlx::query_scalar("SELECT version()")
-        .fetch_one(db.get_ref())
-        .await;
-    result.unwrap_or_else(|e| format!("Error: {:?}", e))
-}
+
