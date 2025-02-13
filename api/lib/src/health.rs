3008c7cd2a393f0e675c8da7585c4121577a98a2
Vickouma77 <Vickprestone20@gmail.com>
2025-02-13 17:11:28 +0300

    refactor(api): use configure
    Signed-off-by: Vickouma77 <Vickprestone20@gmail.com>

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -1,7 +1,12 @@
-use actix_web::{get, HttpResponse};
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpResponse,
+};
+pub fn service(cfg: &mut ServiceConfig) {
+    cfg.route("/health", web::get().to(health));
+}
 
 
-#[get("/health")]
 async fn health() -> HttpResponse {
     HttpResponse::Ok()
         .append_header(("version", "0.0.1"))
