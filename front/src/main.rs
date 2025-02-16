0fa6dc025bb4fc1ed8109462707fcc41dd461aaf
Vickouma77 <Vickprestone20@gmail.com>
2025-02-16 19:13:16 +0300

    feat(front): update Tailwind integration and add main application component

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,3 +1,16 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // Launch the web application using the App component as the root.
+    dioxus_web::launch(App);
+}
+
+// Define a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, Vick!"
+        }
+    })
 }
