8db917e4bd5050c1949442dc19b68b150063f306
Vickouma77 <Vickprestone20@gmail.com>
2025-02-17 15:29:24 +0300

    refactor(front): move CSS asset declarations into the App component for better scope management

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,14 +1,14 @@
 use dioxus::prelude::*;
 
-const MAIN_CSS: Asset = asset!("/assets/main.css");
-const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
-
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
+    static MAIN_CSS: Asset = asset!("/assets/main.css");
+    static TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
     rsx! {
         document::Link { rel: "stylesheet", href: MAIN_CSS } 
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
